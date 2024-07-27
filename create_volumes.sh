1e5301c7adac671e67e4f50322859ad723f45da4
Sreejith Narayanan <sreejithhere@gmail.com>
2024-07-28 00:21:16 +0530

    Mostly working execpt for traefik

--- create_volumes.sh
+++ create_volumes.sh
@@ -51,16 +51,25 @@ docker volume create -d local \
 	-o o=bind \
 	-o device=/home/sreejith/projects/data/smarttv/plex/config \
 	plex_config
-#docker volume create common_video_tv:/home/sreejith/projects/data/smarttv/videos/tv
+docker volume create -d local \
+	-o type=none \
+	-o o=bind \
+	-o device=/home/sreejith/projects/data/smarttv/prowlarr/config \
+	prowlarr_config
 
-#docker volume create common_video_movies:/home/sreejith/projects/data/smarttv/videos/movies
+docker volume create -d local \
+	-o type=none \
+	-o o=bind \
+	-o device=/home/sreejith/projects/data/smarttv/flaresolverr/config \
+	flaresolverr_config
 
-#docker volume create common_video_others:/home/sreejith/projects/data/smarttv/videos/others
-
-#docker volume_create qb_config:/home/sreejith/projects/data/smarttv/qbittorrent/config
-
-#docker volume create sonarr_config:/home/sreejith/projects/data/smarttv/sonarr_config
-
-#docker volume create radarr_config:/home/sreejith/projects/data/smarttv/radarr_config
-
-#docker volume create overseerr_config:/home/sreejith/projects/data/smarttv/overseerr_config
+docker volume create -d local \
+	-o type=none \
+	-o o=bind \
+	-o device=/home/sreejith/projects/data/smarttv/traefik/config \
+	traefik_config
+docker volume create -d local \
+	-o type=none \
+	-o o=bind \
+	-o device=/home/sreejith/projects/data/smarttv/bazarr/config \
+	bazarr_config
